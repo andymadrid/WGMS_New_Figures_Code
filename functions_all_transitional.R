4ae253756436e9d2b17def9b74c8dbd021e9c746
Andy Madrid <76069657+andymadrid@users.noreply.github.com>
2023-11-15 13:42:27 -0600

    Update functions_all_transitional.R

--- functions_all_transitional.R
+++ functions_all_transitional.R
@@ -383,7 +383,7 @@ harmonic_pvalue_routine <- function(loci.gr, features.gr, alpha){
     group_by(gene_name) %>%
     dplyr::summarize(
       N.CpGs = n(),
-      N.DMPs = sum(lfdr < alpha),
+      N.DMPs = sum(lfdr < alpha & abs(pi.diff.mci.ctrl) > 0.0125 & abs(pi.diff.load.mci) > 0.0125),
       HarmonicMeanPval = harmonicmeanp::p.hmp(pval, L = N.CpGs)
     ) %>%
     dplyr::mutate(HarmonicMeanPval = pmin(HarmonicMeanPval, 1)) %>%
@@ -400,6 +400,55 @@ harmonic_pvalue_routine <- function(loci.gr, features.gr, alpha){
   hmp.df
 }
 
+harmonic_pvalue_routine_hyper_hypo <- function(loci.gr, features.gr, alpha){
+  df <- make_df_from_two_overlapping_granges(loci.gr, features.gr)
+
+  # Hyper run
+  hmp.df <- df %>%
+    group_by(gene_name) %>%
+    dplyr::summarize(
+      N.CpGs = n(),
+      N.DMPs = sum(lfdr < alpha & pi.diff.mci.ctrl > 0.0125 & pi.diff.load.mci > 0.0125),
+      HarmonicMeanPval = harmonicmeanp::p.hmp(pval, L = N.CpGs)
+    ) %>%
+    dplyr::mutate(HarmonicMeanPval = pmin(HarmonicMeanPval, 1)) %>%
+    dplyr::mutate(HarmonicMeanPval = pmax(HarmonicMeanPval, 0))
+
+  # FDR calculation
+  fdr.out <- fdrtool::fdrtool(x = hmp.df$HarmonicMeanPval, statistic = "pvalue", plot = F)
+
+  # Pack together
+  hmp.df$lfdr <- fdr.out$lfdr
+  hmp.df$qval <- fdr.out$qval
+
+  # Output
+#  hmp.df
+  hmp.df.hyper <- hmp.df
+
+  # Hypo run
+  hmp.df <- df %>%
+    group_by(gene_name) %>%
+    dplyr::summarize(
+      N.CpGs = n(),
+      N.DMPs = sum(lfdr < alpha & pi.diff.mci.ctrl < -0.0125 & pi.diff.load.mci < -0.0125),
+      HarmonicMeanPval = harmonicmeanp::p.hmp(pval, L = N.CpGs)
+    ) %>%
+    dplyr::mutate(HarmonicMeanPval = pmin(HarmonicMeanPval, 1)) %>%
+    dplyr::mutate(HarmonicMeanPval = pmax(HarmonicMeanPval, 0))
+
+  # FDR calculation
+  fdr.out <- fdrtool::fdrtool(x = hmp.df$HarmonicMeanPval, statistic = "pvalue", plot = F)
+
+  # Pack together
+  hmp.df$lfdr <- fdr.out$lfdr
+  hmp.df$qval <- fdr.out$qval
+
+  # Output
+#  hmp.df
+  hmp.df.hypo <- hmp.df
+  hmp.df <- list("hyper" = hmp.df.hyper, "hypo" = hmp.df.hypo)
+  hmp.df
+}
 
 
 # -------------------------------------------------------------------------
